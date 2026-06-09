58031c8f933a20ae9d57478e4c533d2b0afdca32
temmytorpe312-pixel <temmytorpe312@gmail.com>
2026-06-09 17:41:46 +0100

    Updated Footers

---
+++ src/components/Footer.jsx
@@ -0,0 +1,55 @@
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer-container">
+        <div className="footer-top">
+          <div className="footer-about">
+            <h3>About</h3>
+            <p>
+              We are a group of 14 students from TS Academy, passionate about web
+              development and design. Our mission is to create engaging and
+              informative web experiences that educate users about our solar
+              system through data visualization.
+            </p>
+          </div>
+
+          <div className="footer-members">
+            <h4>Our team members include:</h4>
+           <div className="members-grid">
+  <span>Emmanuel</span>
+  <span>Bashir</span>
+  <span>Sasere</span>
+  <span>Akinmolayan</span>
+
+  <span>Modester</span>
+  <span>Otaru</span>
+  <span>Oriowo</span>
+  <span>Akojenu</span>
+
+  <span>Adebola</span>
+  <span>Asuquo</span>
+  <span>Idowu</span>
+  <span>Ogba</span>
+
+  <span>Ajunwa</span>
+  <span>Mesole</span>
+</div>
+          </div>
+        </div>
+
+        <div className="footer-line"></div>
+
+        <div className="footer-bottom">
+          <div className="footer-left">
+            <p>©2026 Design by Amaka & Ifeoma A.</p>
+            <p>Built by Group13. All rights reserved.</p>
+          </div>
+
+          <div className="footer-right">TSAcademy</div>
+        </div>
+      </div>
+    </footer>
+  );
+}
