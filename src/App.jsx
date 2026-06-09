16936d539a463bfedc9e13654e9b84b42a7a660a
akojenuserahuser.name <akojenuserah2@gmail.com>
2026-06-09 15:44:44 +0100

    Add the PlanetCard section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Hero from "./components/Hero";
 import Header from "./components/Header";
+import PlanetCard from "./components/PlanetCard"; 
 
 function App() {
   return (
     <div className="app-container">
       <Header />
       <Hero />
+      <PlanetCard />
     </div>
   );
 }
