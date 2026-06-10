20af5b501861d254a30512dd12224310a2f18bfe
Emi98nat <ihechukwuezulumike@gmail.com>
2026-06-10 16:49:02 +0100

    add Datatable component

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,21 @@ import "./App.css";
 
 import Hero from "./components/Hero";
 import Header from "./components/Header";
-import PlanetCard from "./components/PlanetCard"; 
 import VideoSection from "./components/VideoSection";
 import ContactForm from "./components/ContactForm";
-import Footer from "./components/Footer";
+import Datatable from "./components/Datatable";
+import PlanetSection from "./components/PlanetCard"
+import Footer from "./components/Footer"
 function App() {
   return (
     <div className="app-container">
       <Header />
       <Hero />
-     <VideoSection />
-     <PlanetCard />
-      <ContactForm />
-      <Footer />
+      <VideoSection />
+      <PlanetSection />
+      <Datatable />   
+     <ContactForm />
+    <Footer />
     </div>
   );
 }
