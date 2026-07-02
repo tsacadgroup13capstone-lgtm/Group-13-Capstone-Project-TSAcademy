9cb437728056a34381139a0d5ba1aa012595a6a9
tsacadgroup13capstone-lgtm <tsacadgroup13capstone@gmail.com>
2026-07-02 19:56:25 +0100

    make all necessary changes feedback 37 modify   package-lock.json modify   src/components/Footer.jsx modify   src/components/Hero.css modify   src/components/PlanetCard.jsx modify   src/components/PlanetCard.css modify   src/components/VideoSection.jsx modify   src/index.css

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -20,51 +20,133 @@ export default function Footer() {
             <h4>Our team members include:</h4>
             <div className="members-grid">
               <span>
-                <a href="https://github.com/Emi98nat/" target="_blank" rel="noopener noreferrer">Emmanuel</a>
+                <a
+                  href="https://github.com/Emi98nat/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Emmanuel
+                </a>
               </span>
               <span>
-                <a href="https://github.com/BashirSaka/RESPONSIVE-PROJECT-FLEXBOX.git" target="_blank" rel="noopener noreferrer">
+                <a
+                  href="https://github.com/BashirSaka/RESPONSIVE-PROJECT-FLEXBOX.git"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
                   Bashir
                 </a>
               </span>
               <span>
-                <a href="https://github.com/Rebecca0-12" target="_blank" rel="noopener noreferrer">Sasere</a>
+                <a
+                  href="https://github.com/Rebecca0-12"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Sasere
+                </a>
               </span>
               <span>
-                <a href="https://github.com/mideakinmolayan" target="_blank" rel="noopener noreferrer">Akinmolayan</a>
-              </span>
-
-              <span>
-                <a href="https://github.com/moddyoge-cloud" target="_blank" rel="noopener noreferrer">Modester</a>
-              </span>
-              <span>
-                <a href="https://github.com/Orenyi" target="_blank" rel="noopener noreferrer">Otaru</a>
-              </span>
-              <span>
-                <a href="https://github.com/Omobolanle27" target="_blank" rel="noopener noreferrer">Oriowo</a>
-              </span>
-              <span>
-                <a href="https://github.com/akojenuserah" target="_blank" rel="noopener noreferrer">Akojenu</a>
-              </span>
-
-              <span>
-                <a href="https://github.com/Adebola09" target="_blank" rel="noopener noreferrer">Adebola</a>
-              </span>
-              <span>
-                <a href="https://github.com/Battleaxe-R" target="_blank" rel="noopener noreferrer">Asuquo</a>
-              </span>
-              <span>
-                <a href="https://github.com/idowuoriyomi808-ai" target="_blank" rel="noopener noreferrer">Idowu</a>
-              </span>
-              <span>
-                <a href="https://github.com/ogba07" target="_blank" rel="noopener noreferrer">Ogba</a>
+                <a
+                  href="https://github.com/mideakinmolayan"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Akinmolayan
+                </a>
               </span>
 
               <span>
-                <a href="https://github.com/Favourh" target="_blank" rel="noopener noreferrer">Ajunwa</a>
+                <a
+                  href="https://github.com/moddyoge-cloud"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Modester
+                </a>
               </span>
               <span>
-                <a href="https://github.com/temmytorpe312-pixel" target="_blank" rel="noopener noreferrer">Mesole</a>
+                <a
+                  href="https://github.com/Orenyi"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Otaru
+                </a>
+              </span>
+              <span>
+                <a
+                  href="https://github.com/Omobolanle27"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Oriowo
+                </a>
+              </span>
+              <span>
+                <a
+                  href="https://github.com/akojenuserah"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Akojenu
+                </a>
+              </span>
+
+              <span>
+                <a
+                  href="https://github.com/Adebola09"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Adebola
+                </a>
+              </span>
+              <span>
+                <a
+                  href="https://github.com/Battleaxe-R"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Asuquo
+                </a>
+              </span>
+              <span>
+                <a
+                  href="https://github.com/idowuoriyomi808-ai"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Idowu
+                </a>
+              </span>
+              <span>
+                <a
+                  href="https://github.com/ogba07"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Ogba
+                </a>
+              </span>
+
+              <span>
+                <a
+                  href="https://github.com/Favourh"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Ajunwa
+                </a>
+              </span>
+              <span>
+                <a
+                  href="https://github.com/temmytorpe312-pixel"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Mesole
+                </a>
               </span>
             </div>
           </div>
@@ -75,14 +157,44 @@ export default function Footer() {
         <div className="footer-bottom">
           <div className="footer-left">
             <p>
-              ©2026 Design by <a href="https://amakandukwu.com/" target="_blank" rel="noopener noreferrer">Amaka</a> &{" "}
-              <a href="https://www.linkedin.com/in/ifeomaokocha" target="_blank" rel="noopener noreferrer">Ifeoma A.</a>
+              ©2026 Design by{" "}
+              <a
+                href="https://amakandukwu.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Amaka
+              </a>{" "}
+              &{" "}
+              <a
+                href="https://www.linkedin.com/in/ifeomaokocha"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Ifeoma A.
+              </a>
             </p>
-            <p>Built by Group13. All rights reserved.</p>
+            <p>
+              Built by{" "}
+              <a
+                href="https://github.com/tsacadgroup13capstone-lgtm/Group-13-Capstone-Project-TSAcademy"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Group13
+              </a>
+              . All rights reserved.
+            </p>
           </div>
 
           <div className="footer-right">
-            <a href="https://tsacademyonline.com/" target="_blank" rel="noopener noreferrer">TSAcademy</a>
+            <a
+              href="https://tsacademyonline.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              TSAcademy
+            </a>
           </div>
         </div>
       </div>
